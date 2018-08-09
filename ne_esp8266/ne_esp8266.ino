2e50b21b2798d78b1898b184d60c1c9c26bcff87
ojs <contact@ollyjsmith.com>
2018-08-09 18:13:49 +0100

    simplify setup - example of 1 pitch square per packet type. Add buttons - trigger low waves with functional envelopes

--- ne_esp8266/ne_esp8266.ino
+++ ne_esp8266/ne_esp8266.ino
@@ -1,6 +1,6 @@
-//#define SAMPLE_RATE 16000 
+// can use other sample rates - try 44100 for higher 
+// quality or 16000 to allow for some more complex processing
 #define SAMPLE_RATE 22050 
-//#define SAMPLE_RATE 44100
 
 #include <Arduino.h>
 
@@ -10,28 +10,34 @@
 #include "./VoiceTriangle.h"
 #include "./VoiceSquare.h"
 #include "./VoiceSaw.h"
-#include "./VoiceWavetable.h"
-#include "./VoiceSample.h"
 
 extern "C" {
   // This includes the base functionality for the ESP8266
   #include "user_interface.h"
 }
 
+long t;
+
 int wifiChannel = 1;
 
 int lastChannelChangeAt = 0;
 int channelChangeTimeout = 5000;
 
-VoiceTriangle triangle1( SAMPLE_RATE / 60 );
-VoiceTriangle triangle2( SAMPLE_RATE / 100 );
+// buttons. LOW is active/pressed (because of pullup stuff)
+int BTN_0  = HIGH;
+int pBTN_0 = HIGH;
+int BTN_1  = HIGH;
+int pBTN_1 = HIGH;
 
-long lastPktAt = 0;
-long pktTimeout = 50;
+VoiceSquare sound1( SAMPLE_RATE / 220 );
+VoiceSquare sound2( SAMPLE_RATE / 440 );
+VoiceSquare sound3( SAMPLE_RATE / 880 );
 
-int voiceIndex = 0;
+VoiceTriangle sound4( SAMPLE_RATE / 55 );
+VoiceSaw sound5( SAMPLE_RATE / 110 );
 
 void onPacket( uint8_t *buf, uint16_t len ){
+  t = millis();
   /* calculate type and distance of packet */
   double distance = 1000000; // something high that it will never be
   int type = 0;
@@ -51,35 +57,25 @@ void onPacket( uint8_t *buf, uint16_t len ){
     distance = wifi_calculatePacketDist( sniffer->rx_ctrl.rssi, wifiChannel  );    
   }
 
-  
-  if( type == 1 || type == 2 ){  // triggered by packets that aren't Device Frames 
-    if( voiceIndex == 0 ){
-      
-      if( triangle1.adsr.env <= 0 ){ //retrigger if( complete );
-        triangle1.setCycle( SAMPLE_RATE / 60 );
-        //triangle1.adsr.triggerEnv( SAMPLE_RATE / 25 );
-        triangle1.adsr.triggerEnv( distance * 100 );
-      } else { //pitch down gradually
-        triangle1.setCycle( triangle1.cycleLength - 1 ); //pitch up
-      }
-      
-      voiceIndex++;
-    } else {
-       if( triangle2.adsr.env <= 0 ){ //retrigger if( complete );
-        triangle2.setCycle( SAMPLE_RATE / 100 );
-        //triangle2.adsr.triggerEnv( SAMPLE_RATE / 25 );
-        triangle2.adsr.triggerEnv( distance * 100 );
-      } else { //pitch down gradually
-        triangle2.setCycle( triangle2.cycleLength + 1 ); // pitch down
-      }
-      voiceIndex = 0;
-    }    
-  } 
-  
+
+  if( type == 1 ){
+    sound1.adsr.triggerEnv();
+  } else if( type == 2 ){
+    sound2.adsr.triggerEnv();
+  } else if( type == 3 ){
+    sound3.adsr.triggerEnv();
+  }
 }
 
 void setup() {
 
+  //pin setup
+  pinMode(0, INPUT);  // GPIO0 -> BTN_0
+  
+  pinMode(1, FUNCTION_3); // TX -> GPIO1 -> BTN_1
+  pinMode(1, INPUT);
+  
+  
   delay( 200 );
 
   //WiFi Setup
@@ -92,24 +88,55 @@ void setup() {
   delay( 5 );
   
   audio_setup();
-  triangle1.adsr.setADSR( SAMPLE_RATE / 50, SAMPLE_RATE / 50, 0.7, SAMPLE_RATE / 50 );
-  triangle2.adsr.setADSR( SAMPLE_RATE / 50, SAMPLE_RATE / 50, 0.6, SAMPLE_RATE / 50 );
+  sound1.adsr.setADSR( SAMPLE_RATE / 100, SAMPLE_RATE / 100, 0.8, SAMPLE_RATE / 100 );
+  sound2.adsr.setADSR( SAMPLE_RATE / 100, SAMPLE_RATE / 100, 0.8, SAMPLE_RATE / 100 );
+  sound3.adsr.setADSR( SAMPLE_RATE / 100, SAMPLE_RATE / 100, 0.8, SAMPLE_RATE / 100 );
+  
+  sound4.adsr.setADSR( SAMPLE_RATE / 4, SAMPLE_RATE / 10, 0.9, SAMPLE_RATE );
+  sound5.adsr.setADSR( SAMPLE_RATE / 4, SAMPLE_RATE / 10, 0.9, SAMPLE_RATE );
+
 }
 
 int sampCount = 0;
 
 void loop() {
-  yield();
-  // if there's space in the audio buffer, add a sample
-  if( i2s_available() > 0 ){ 
-    triangle1.tick();
-    triangle2.tick();
-    audio_writeDAC( ( triangle1.sample + triangle2.sample ) / 2 );
+  t = millis();
+
+  BTN_0 = digitalRead( 0 );
+  BTN_1 = digitalRead( 1 );
+
+  if( BTN_0 != pBTN_0 ){
+    if( BTN_0 == LOW ){
+      sound4.adsr.startEnv();
+    } else {
+      sound4.adsr.releaseEnv();
+    }
+  }
+  if( BTN_1 != pBTN_1 ){
+    if( BTN_1 == LOW ){
+      sound5.adsr.startEnv();
+    } else {
+      sound5.adsr.releaseEnv();
+    }
   }
   
   yield();
-  // step through wifi channels
-  long t = millis();
+  // if there's space in the audio buffer, add a sample
+  if( i2s_available() > 0 ){ 
+    sound1.tick();
+    sound2.tick();
+    sound3.tick();
+
+    sound4.tick();
+    sound5.tick();
+   
+    //audio_writeDAC( ((sound1.sample + sound2.sample + sound3.sample) /10 )+ ((sound4.sample + sound5.sample) / 2) );
+    audio_writeDAC( (sound4.sample + sound5.sample) / 2);
+  }
+  
+  yield();
+  
+  // step through wifi channels  
   if( t - lastChannelChangeAt > channelChangeTimeout ){
     lastChannelChangeAt = t;
     wifiChannel++;
@@ -120,4 +147,7 @@ void loop() {
 
     wifi_set_channel( wifiChannel );
   }
+
+  pBTN_0 = BTN_0;
+  pBTN_1 = BTN_1;
 }
