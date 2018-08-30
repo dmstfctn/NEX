47190d4447908b384aaf349efbff1856bb1aabad
ojs <contact@ollyjsmith.com>
2018-08-30 13:04:00 +0100

    remove unused oscils and includes, just 3 samples

--- ne_esp8266_mozzi/ne_esp8266_mozzi.ino
+++ ne_esp8266_mozzi/ne_esp8266_mozzi.ino
@@ -7,16 +7,8 @@ extern "C" {
   #include "user_interface.h"
 }
 
-#include <mozzi_midi.h> // for mtof
-#include <mozzi_fixmath.h>
-
-#include <Oscil.h>
-#include <tables/saw2048_int8.h> // table for Oscils to play
-//#include <tables/whitenoise8192_int8.h>
-#include <ADSR.h>
-
-#include "./NE_ReverbTank.h"
-#include <LowPassFilter.h>
+#include <Sample.h>
+#include "./samples/CLICK_SR32768.h"
 
 unsigned long t;
 int wifiChannel = 1;
@@ -31,26 +23,16 @@ int pBTN_1 = HIGH;
 
 boolean button0 = false;
 boolean button1 = false;
+boolean button0_latch = false;
+boolean button1_latch = false;
 
-Oscil<SAW2048_NUM_CELLS, AUDIO_RATE> sound1( SAW2048_DATA );
-ADSR <CONTROL_RATE, AUDIO_RATE> env1;
-boolean noteOn1 = false;
-long noteOnAt1 = 0;
+Sample<CLICK_NUM_CELLS, AUDIO_RATE> sound1( CLICK_DATA );
+Sample<CLICK_NUM_CELLS, AUDIO_RATE> sound2( CLICK_DATA );
+Sample<CLICK_NUM_CELLS, AUDIO_RATE> sound3( CLICK_DATA );
 
-Oscil<SAW2048_NUM_CELLS, AUDIO_RATE> sound2( SAW2048_DATA );
-ADSR <CONTROL_RATE, AUDIO_RATE> env2;
-boolean noteOn2 = false;
-long noteOnAt2 = 0;
+uint8_t currentSound = 0;
+int CLICK_BASE_FREQ = ((float) CLICK_SAMPLERATE / (float) CLICK_NUM_CELLS);
 
-Oscil<SAW2048_NUM_CELLS, AUDIO_RATE> sound3( SAW2048_DATA );
-ADSR <CONTROL_RATE, AUDIO_RATE> env3;
-boolean noteOn3 = false;
-long noteOnAt3 = 0;
-
-NE_ReverbTank reverb;
-LowPassFilter lowPass;
-
-int noteOnLength = 1000; //in micros
 
 void onPacket( uint8_t *buf, uint16_t len ){
   t = mozziMicros(); //instead of millis - mozzi disables this (I think)
@@ -72,27 +54,22 @@ void onPacket( uint8_t *buf, uint16_t len ){
     struct sniffer_buf *sniffer = (struct sniffer_buf*) buf;
     distance = wifi_calculatePacketDist( sniffer->rx_ctrl.rssi, wifiChannel  );    
   }
-  //if( distance < 3 ){
-    if( type == 1 ){
-      if( !noteOn1 ){
-        env1.noteOn();
-        noteOnAt1 = t;
-        noteOn1 = true;
-      }
-    } else if( type == 2 ){
-      if( !noteOn2 ){
-        env2.noteOn();
-        noteOnAt2 = t;
-        noteOn2 = true;
-      }
-    } else if( type == 3 ){
-      if( !noteOn3 ){
-        env3.noteOn();
-        noteOnAt3 = t;
-        noteOn3 = true;
-      }
-    }
-  //}
+
+  if( currentSound == 0 ){
+    sound1.setFreq( CLICK_BASE_FREQ - (float) distance );
+    sound1.start();
+  } else if( currentSound == 1 ){
+    sound2.setFreq( CLICK_BASE_FREQ - (float) distance );
+    sound2.start();
+  } else if( currentSound == 2 ){
+    sound3.setFreq( CLICK_BASE_FREQ - (float) distance );
+    sound3.start();
+  }
+
+  currentSound++;
+  if( currentSound > 2 ){
+    currentSound = 0;
+  }
  
 }
 
@@ -116,25 +93,10 @@ void setup() {
   delay( 5 );
   
   startMozzi( CONTROL_RATE );
+  sound1.setFreq( CLICK_BASE_FREQ );
+  sound2.setFreq( CLICK_BASE_FREQ );
+  sound3.setFreq( CLICK_BASE_FREQ );
 
-  sound1.setFreq( mtof(57.f-24.f) );
-  env1.setADLevels( 240, 50 ); //0 - 255
-  env1.setTimes( 5, 5, 5, 5 ); //milliseconds
-  env1.update();
-  
-  sound2.setFreq( mtof(69.f-24.f) );
-  env2.setADLevels( 240, 50 ); //0 - 255
-  env2.setTimes( 5, 5, 5, 5 ); //milliseconds
-  env2.update();
-  
-  sound3.setFreq( mtof(81.f-24.f) );
-  env3.setADLevels( 240, 50 ); //0 - 255
-  env3.setTimes( 5, 5, 5, 5 ); //milliseconds
-  env3.update();
-
-  //reverb.setFeebackLevel( 80 );
-  lowPass.setCutoffFreq( 102 ); //0 = 0, 255 = AUDIO_RATE/2
-  lowPass.setResonance( 110 );
 }
 
 void updateControl(){
@@ -145,20 +107,18 @@ void updateControl(){
   if( BTN_0 != pBTN_0 ){
     if( BTN_0 == LOW ){ // button 0 pressed
       button0 = true;
-      env1.noteOn();
     } else { // button 0 released
       button0 = false;
-      env1.noteOff();
+      button0_latch = !button0_latch;
     }
   }
   if( BTN_1 != pBTN_1 ){
     if( BTN_1 == LOW ){ // button 1 pressed
-      button1 = true;
-      env2.noteOn();
+      button1 = true;      
     } else { // button 1 released
       // nothing
       button1 = false;
-      env2.noteOff();
+      button1_latch = !button1_latch;
     }
   }
   // step through wifi channels  
@@ -173,55 +133,16 @@ void updateControl(){
     wifi_set_channel( wifiChannel );
   }
 
-  if( noteOn1 && t - noteOnAt1 > noteOnLength ){
-    if( t - noteOnAt1 > noteOnLength*2 ){
-      noteOn1 = false;
-    } else if( t - noteOnAt1 > noteOnLength ){
-      env1.noteOff();    
-    }
-  }
-  if( noteOn2 && t - noteOnAt2 > noteOnLength ){
-    if( t - noteOnAt2 > noteOnLength*2 ){
-      noteOn2 = false;
-    } else if( t - noteOnAt2 > noteOnLength ){
-      env2.noteOff();    
-    }
-  }
-  if( noteOn3  && t - noteOnAt3 > noteOnLength ){
-    if( t - noteOnAt3 > noteOnLength*2 ){
-      noteOn3 = false;
-    } else if( t - noteOnAt3 > noteOnLength ){
-      env3.noteOff();    
-    }
-  }
-
-  env1.update();
-  env2.update();
-  env3.update();
   pBTN_0 = BTN_0;
   pBTN_1 = BTN_1;
 }
 
 int updateAudio(){
-  int out = ((int) sound1.next() * env1.next() >> 8); 
-  out += ((int) sound2.next() * env2.next() >> 8);
-  out += ((int) sound3.next() * env3.next() >> 8);
+  int out = (int) sound1.next();
+  out +=  (int) sound2.next();
+  out +=  (int) sound3.next();
 
-  if( button0 ){
-    out = lowPass.next( out );
-  }
-  
-  if( button1 ){    
-    int rvb = reverb.next( out );
-    out = (out + (rvb>>2));        
-  } 
-  
-
-
-  //out = out >> 2; //same as /4 ?
-
-  //return (int16_t) map( out, -128, 127, -32768, 32767 );
-  return out << 8; //same as above? (i.e. mapping from 8bit int to 16bit int)
+  return out << 8; //the << 8 should map from 8bit int to 16bit int
 }
 
 void loop(){
